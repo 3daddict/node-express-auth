4fe634c06829fee38d1f5b62e2bb280a4f8fba4e
Michael Salvati <print@3daddict.com>
2018-12-05 10:23:15 -0800

    mongodb setup, controllers setup and server updated

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+//DB Setup
+mongoose.connect("mongodb://localhost:auth/auth", { useNewUrlParser: true });
 
 //App Setup
 app.use(morgan('combined'));
