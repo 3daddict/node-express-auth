b938ef5d0c8bd74994424240c78bad23d204e9f1
Michael Salvati <print@3daddict.com>
2018-12-06 13:13:38 -0800

    cors installed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //DB Setup
 mongoose.connect("mongodb://localhost:auth/auth", { useNewUrlParser: true });
@@ -12,6 +13,7 @@ mongoose.set('useCreateIndex', true);
 
 //App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json( { type: '*/*' } ));
 router(app);
 
