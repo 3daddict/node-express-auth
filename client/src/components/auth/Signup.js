7e123f4e0fe4481df4f6ecededb4867f9fabd307
Michael Salvati <print@3daddict.com>
2018-12-06 12:58:16 -0800

    connected client to server for auth installaed cors

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 class Signup extends Component {
 
     onSubmit = (formProps) => {
-        console.log(formProps);
+        this.props.signup(formProps);
     };
 
     render() {
@@ -27,4 +30,7 @@ class Signup extends Component {
     }
 }
 
-export default reduxForm({ form: 'signup' })(Signup);
+export default compose(
+    connect(null, actions),
+    reduxForm({ form: 'signup' })
+)(Signup);
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
     <BrowserRouter>
         <App>
             <Route exact path="/" component={Welcome} />
