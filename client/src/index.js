2fbe071283fb0fe3df84d56588d722ba8eb02efb
Michael Salvati <print@3daddict.com>
2018-12-06 09:58:58 -0800

    redux configured

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 ReactDOM.render(
+    <Provider store={createStore(reducers, {})}>
     <BrowserRouter>
         <App>
             <Route exact path="/" component={Welcome} />
+            <Route path="/signup" component={Signup} />
         </App>
-    </BrowserRouter>,
+    </BrowserRouter>
+    </Provider>,
     document.querySelector('#root')
 );
